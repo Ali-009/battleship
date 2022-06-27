2aed5ef9caf45181e0069982f2f0bbf1a613e861
AliTinai <ali-009@windowslive.com>
2022-06-27 17:16:31 +0400

    Finished testing the refactored occupyCells

--- test/ship-factory.test.js
+++ test/ship-factory.test.js
@@ -38,25 +38,25 @@ test('isSunk Function Test 2', () => {
 test('Getting occupied cells given only shipCoordinates',
 () => {
     const destroyer = ShipFactory.createShip(3, ['A1', 'A3']);
-    expect(destroyer.occupyCells()).toEqual(['A1','A3','A2']);
+    expect(destroyer.occupiedCells).toEqual(['A1','A3','A2']);
 });
 
 test('occupyCells test 2', () =>{
     const carrier = ShipFactory.createShip(5, ['D4','D8']);
-    expect(carrier.occupyCells()).toEqual(['D4','D8','D5','D6','D7']);
+    expect(carrier.occupiedCells).toEqual(['D4','D8','D5','D6','D7']);
 });
 
 test('occupyCells vertical test 1', () => {
     const carrier = ShipFactory.createShip(5, ['B6','F6']);
-    expect(carrier.occupyCells()).toEqual(['B6','F6','C6','D6','E6']);
+    expect(carrier.occupiedCells).toEqual(['B6','F6','C6','D6','E6']);
 });
 
 test('occupyCells vertical test2', () => {
     const patrolBoat = ShipFactory.createShip(2, ['D1','E1']);
-    expect(patrolBoat.occupyCells()).toEqual(['D1','E1']);
+    expect(patrolBoat.occupiedCells).toEqual(['D1','E1']);
 });
 
 test('Invalid Ship Coordinates Test', () => {
-    const weirdShip = ShipFactory.createShip(5, ['D4','J2']);
-    expect(() => weirdShip.occupyCells()).toThrow(new Error ('Invalid Ship Coordinates'));
+    expect(() => ShipFactory.createShip(5, ['D4','J2']))
+    .toThrow(new Error ('Invalid Ship Coordinates'));
 })
