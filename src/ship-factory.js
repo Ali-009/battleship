bd54bef19c31e1c3e276e380e292437f90e6da27
AliTinai <ali-009@windowslive.com>
2022-06-27 17:15:44 +0400

    Refactored the occupyCells function

--- src/ship-factory.js
+++ src/ship-factory.js
@@ -2,6 +2,55 @@
 function createShip(shipLength, shipCoordinates){
     
     let hitSpots = [];
+    //A function to create an array of occupiedCells
+    const occupyCells = function (shipCoordinates){
+        let occupiedCells = shipCoordinates;
+        const columnNotation = ['A','B','C','D','E','F','G'
+            ,'H','I','J'];
+        let startPoint;
+        let endPoint;
+        //The coordinates on the x-axis
+        let x1 = shipCoordinates[0].charAt(0);
+        let x2 = shipCoordinates[1].charAt(0);
+
+        //The coordinates on the y-axis
+        let y1 = +shipCoordinates[0].charAt(1);
+        let y2 = +shipCoordinates[1].charAt(1);
+
+        //If the ship has a vertical orientation
+        if(x1 === x2){
+            if(y2 > y1){
+                startPoint = y1;
+                endPoint = y2;
+            } else {
+                startPoint = y2;
+                endPoint = y1;
+            }
+            for(let i = startPoint + 1; i < endPoint; i++){
+                occupiedCells.push(`${x1 + i.toString()}`);
+            }
+            return occupiedCells;
+        } else if(y1 === y2){
+            //The ship has a horizontal orientation
+            //The logic here is similar to the above code block
+            //The code below changes the alphabetic notation into a numerical one that we can iterate over
+            if(columnNotation.indexOf(x2) > columnNotation.indexOf(x1)){
+                startPoint = columnNotation.indexOf(x1)
+                endPoint = columnNotation.indexOf(x2);
+            } else {
+                startPoint = columnNotation.indexOf(x2);
+                endPoint = columnNotation.indexOf(x1);
+            }
+            for(let i = startPoint + 1; i < endPoint; i++){
+                occupiedCells.push(`${columnNotation[i] + y1}`);
+            }
+                return occupiedCells;
+            } else {
+                throw new Error('Invalid Ship Coordinates');
+            }
+    }
+
+    const occupiedCells = occupyCells(shipCoordinates);
     const hit = function(position){
         return hitSpots.push(position);
     }
@@ -10,48 +59,8 @@ function createShip(shipLength, shipCoordinates){
         ? true
         : false;
     }
-    const occupyCells = function(){
-        let occupiedCells = this.shipCoordinates;
-        const rowNotation = ['A','B','C','D','E','F','G'
-            ,'H','I','J'];
-        //Initial values for start and end points
-        let startPoint = this.shipCoordinates[0];
-        let endPoint = this.shipCoordinates[1];
-        //check the orientation of the ship (horizontal or vertical)
-        if(this.shipCoordinates[0].charAt(0)
-        === this.shipCoordinates[1].charAt(0)){
-            //See which of the coordinates is larger than the other
-            if(+this.shipCoordinates[0].charAt(1)
-                > +this.shipCoordinates[1].charAt(1)){
-                    startPoint = this.shipCoordinates[1];
-                    endPoint = this.shipCoordinates[0];
-                }
-            const columnStart = +startPoint.charAt(1);
-            const columnEnd = +endPoint.charAt(1);
-            for(let i = columnStart + 1; i < columnEnd; i++){
-                occupiedCells.push(`${this.shipCoordinates[0].charAt(0)}${i}`);
-            }
-            return occupiedCells;
-        } else if(this.shipCoordinates[0].charAt(1)
-            === this.shipCoordinates[1].charAt(1)){
-                //The ship is vertical
-                //The logic here is similar to the above code block
-                if(rowNotation.indexOf(this.shipCoordinates[0].charAt(0)) > rowNotation.indexOf(this.shipCoordinates[1].charAt(0))){
-                    startPoint = this.shipCoordinates[1];
-                    endPoint = this.shipCoordinates[0];
-                }
-                //The code below changes the alphabetic notation into a numerical one that we can iterate over
-                const rowStart = rowNotation.indexOf(startPoint.charAt(0));
-                const rowEnd = rowNotation.indexOf(endPoint.charAt(0));
-                for(let i = rowStart + 1; i < rowEnd; i++){
-                    occupiedCells.push(`${rowNotation[i]}${this.shipCoordinates[0].charAt(1)}`);
-                }
-                return occupiedCells;
-            } else {
-                throw new Error('Invalid Ship Coordinates');
-            }
-    }
-    return {shipLength, shipCoordinates, hit, isSunk, occupyCells};
+
+    return {shipLength, shipCoordinates, hit, isSunk, occupiedCells};
 }
 
 module.exports = {createShip}
