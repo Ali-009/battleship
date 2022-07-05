7ec03fa54085e0bd08b64e65c665ecf51c0b5f22
AliTinai <ali-009@windowslive.com>
2022-07-05 13:29:24 +0400

    The board can now record attacks

--- src/gameboard-factory.js
+++ src/gameboard-factory.js
@@ -4,11 +4,24 @@ const ShipFactory = require('./ship-factory');
 
 function createGameboard(playerName){
     let shipArray = [];
+    let missedAttacks = [];
     const placeShip = function(shipName, shipCoordinates){
         const ship = ShipFactory.createShip(shipName, shipCoordinates);
         shipArray.push(ship);
     }
-    return{playerName, shipArray, placeShip};
+    const receiveAttack = function(attackedCoordinates){
+        //lookup if a ship has been hit by the attack
+        let attackedShip = shipArray.find(
+            (ship) => ship.isOccupying(attackedCoordinates)
+        );
+
+        if(attackedShip){
+            attackedShip.hit(attackedCoordinates);
+        } else {
+            missedAttacks.push(attackedCoordinates);
+        }
+    }
+    return{playerName, shipArray, placeShip, receiveAttack, missedAttacks};
 }
 
 module.exports = {createGameboard};
--- test/gameboard-factory.test.js
+++ test/gameboard-factory.test.js
@@ -18,6 +18,43 @@ test('Test for placing a ship', () => {
     expect(gameBoard.shipArray.length).toBe(3);
 });
 
+test('Test for receiving successful attacks', () => {
+    const gameBoard = GameboardFactory.createGameboard('John');
+    gameBoard.placeShip('Patrol Boat',['B2','B3']);
+    gameBoard.placeShip('Submarine',['C5','E5']);
+    gameBoard.placeShip('Carrier', ['A1','A5']);
+
+    gameBoard.receiveAttack('A4');
+    gameBoard.receiveAttack('A1');
+    gameBoard.receiveAttack('A3');
+    gameBoard.receiveAttack('A2');
+    gameBoard.receiveAttack('A5');
+    /*If all the attacks above are recorded correctly, we expect that the carrier has sunk*/
+    expect(gameBoard.shipArray[2].isSunk()).toBe(true);
+});
+
+test('Test for receiving successful attacks 2', () => {
+    const gameBoard = GameboardFactory.createGameboard('John');
+    gameBoard.placeShip('Patrol Boat',['B2','B3']);
+    gameBoard.placeShip('Submarine',['C5','E5']);
+    gameBoard.placeShip('Carrier', ['A1','A5']);
+
+    gameBoard.receiveAttack('C5');
+    gameBoard.receiveAttack('E5');
+    /*The attacks above are not enough to sink the submarine that occupies the above space*/
+    expect(gameBoard.shipArray[1].isSunk()).toBe(false);
+});
+
+test('Test for missing an attack', () => {
+    const gameBoard = GameboardFactory.createGameboard('John');
+    gameBoard.placeShip('Patrol Boat',['B2','B3']);
+    gameBoard.placeShip('Submarine',['C5','E5']);
+    gameBoard.placeShip('Carrier', ['A1','A5']);
+
+    gameBoard.receiveAttack('J10');
+    gameBoard.receiveAttack('J01');
+    expect(gameBoard.missedAttacks).toEqual(['J10','J01']);
+})
 /*Testing the methods of gameBoard objects will require the use of mocks, as those objects are coupled to ship objects*/
 
 //Before testing the public interface of gameBoard, I should start working on private methods that describe the actual board
